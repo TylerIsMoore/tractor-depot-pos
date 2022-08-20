a43f8372de9823a686c30dc1c54a165805c7e9ab
TylerIsMoore <tylernmoore07@gmail.com>
2022-08-20 07:23:11 -0400

    Utils added

---
+++ client/src/utils/getErrorMessage.js
@@ -0,0 +1,10 @@
+export default function getErrorMessage(error) {
+    const message =
+        (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+    return message || 'Something went wrong.';
+}
